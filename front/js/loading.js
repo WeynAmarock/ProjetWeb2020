65edf62baf9b0269560ab04e17210e26310801a2
Tibooooo <50449880+Tibooooo@users.noreply.github.com>
2020-06-10 14:49:49 +0200

    Update loading.js

--- front/js/loading.js
+++ front/js/loading.js
@@ -13,7 +13,7 @@ function loadCycliste(coureurs){
 
 
 function fillCoureur(coureur){
-    var velo = '<tr><td>'+coureur['mail']+'</td><td><input type="text" value="'+coureur['nom']+'"></td><td><input type="text" value="'+coureur['prenom']+'"></td><td><input type="text" value="'+coureur['club']+'"></td><td><input type="text" value="'+coureur['date_naissance']+'"></td><td><input type="text" value="'+coureur['num_licence']+'"></td><td>'+coureur['categorie']+'</td><td>'+coureur['categorie_categorie_valeur']+'</td><td><button type="submit" class="button" name="btnSub">Valider</button></td></tr>'
+    var velo = '<tr><td>'+coureur['mail']+'</td><td><input type="text" value="'+coureur['nom']+'"></td><td><input type="text" value="'+coureur['prenom']+'"></td><td><input type="text" value="'+coureur['club']+'"></td><td><input type="text" value="'+coureur['date_naissance']+'"></td><td><input type="text" value="'+coureur['num_licence']+'"></td><td>'+coureur['categorie']+'</td><td>'+coureur['categorie_categorie_valeur']+'</td><td><button class="button" name="btnSub" id="'+coureur['mail']+'">Valider</button></td></tr>'
     console.log(velo);
     return velo;
 }
@@ -21,14 +21,29 @@ function fillCoureur(coureur){
 
 ajaxRequest('GET', 'http://prj-cir2-web-api.monposte/php/api.php/cyclistes', loadCycliste);
 
-// ajaxRequest('PUT', 'http://prj-cir2-web-api.monposte/php/api.php/cyclistes/%27,test 
-//   ,'mail=ac@fgt.com & nom=PAUL & prenom=Claude & num_licence='+1234+' & date=2000-5-10 & club=AC GOUESNOU & valide='+1);
 
-// function test(data){
-//     console.log(data);
-// }
+
+// ajaxRequest('PUT', 'http://prj-cir2-web-api.monposte/php/api.php/cyclistes/%27', modifyCycliste);
+
+
+
+
+var element = document.getElementById('cl@team.gt');
+
+var myFunction = function() {
+    print('Vous avez cliqué !');
+};
+
+element.onclick= ajaxRequest('PUT', 'http://prj-cir2-web-api.monposte/php/api.php/cyclistes/%27', myFunction);;
+
+
 
 
 // $('#comment-list').on('click', '.mod', () => {
-// 	ajaxRequest('PUT', 'http://prj-cir2-web-api.monposte/php/api.php/cyclistes/%27, test,'mail=ac@fgt.com & nom=PAUL & prenom=Claude & num_licence='+1234+' & date=2000-5-10 & club=AC GOUESNOU & valide='+1);
+// 	ajaxRequest('PUT', 'php/request.php/comments/' + 
+// 		$(event.target).closest('.mod').attr('value'), () => {
+// 			ajaxRequest('GET', 'php/request.php/comments/?id='+$('#photo').attr('photoid'), loadComments);
+// 		}, 'comment=' + prompt('Nouveau commentaire: '));
 // });
+
+
