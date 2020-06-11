a108c2e004eb6578ee5736de549c3d225025dc86
WeynAmarock <64893257+WeynAmarock@users.noreply.github.com>
2020-06-11 10:57:53 +0200

    Update coureurs.php

--- front/php/coureurs.php
+++ front/php/coureurs.php
@@ -1,38 +1,46 @@
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Coureurs</title>
+        <?php include('include.php'); ?>
         <!-- <link type="text/css" rel="stylesheet" href="css/main.css"> -->
         
     </head>
     <body>
         <?php 
-            include('include.php');           
+          
             include('header.php');
         ?>
+
         <div id="content">
+
             <h1>Liste des coureurs</h1>
             <section>
-                <form id="formulaire" method="POST">
                     <table id="coureurs" class="table table-stripped">
-                        <tr>
-                            <th>Mail</th>
-                            <th>Nom </th>
-                            <th>Prénom </th>
-                            <th>Club</th>
-                            <th>Date de Naissance</th>
-                            <th>Numéro de licence</th>
-                            <th>Categorie Age</th>
-                            <th>Categorie Valeur</th>
-                            
-                        </tr>
-                       
+                        <thead>
+                            <tr>
+                                <th>Mail</th>
+                                <th>Nom </th>
+                                <th>Prénom </th>
+                                <th>Date de Naissance</th>
+                                <th>Numéro de licence</th>
+                                <th>Club</th>
+                                <th>Code INSEE</th>
+                                <th>Valide</th>
+                                <th>Categorie Age</th>
+                                <th>Categorie Valeur</th> 
+                            </tr>
+                        </thead>
+
+                        <tbody id="tbody">
+
+                        </tbody>
+
                     </table>
-                    
-                    
-                </form>
+
+                    <button type="button" id="Modifier">Modifier</button>
+
             </section>
             
         </div>
