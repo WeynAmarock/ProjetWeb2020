7f0a59c559100b1f3d512fa5b34165c2bec9cbeb
Tibooooo <50449880+Tibooooo@users.noreply.github.com>
2020-06-11 09:48:34 +0200

    Update classements.php

--- front/php/classements.php
+++ front/php/classements.php
@@ -9,8 +9,23 @@
     <body>
         <?php 
             include('include.php');
-            include('header.php');
+            // include('header.php');
         ?>
+        <div id="header">
+			
+            <div id="navbar">
+                        
+                <img id="logo" src="../images/velo.png" style=" width: 10%">
+            
+                <ul id="menu">
+                    <li><a href="../index.html">Accueil</a></li>
+                    <li><a href="coureurs.php">Mes Coureurs</a></li>
+                    <li><a href="courses.php">Mes Courses</a></li>
+                    <li><a href="classements.php" class="active">Mes Classements</a></li>
+                </ul>
+
+            </div>
+        </div>
         <div id="contenu">
             <h1>Classements</h1>
         </div>
