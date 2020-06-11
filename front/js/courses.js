f322855518dbefa56726d72dd0227a4c559f00ea
WeynAmarock <64893257+WeynAmarock@users.noreply.github.com>
2020-06-11 17:00:29 +0200

    Update courses.js

--- front/js/courses.js
+++ front/js/courses.js
@@ -1,8 +1,10 @@
 //Pour les informations de course
+
 ajaxRequest('GET', 'http://prj-cir2-web-api.monposte/php/api.php/courses', loadCourse);
+
 function loadCourse(courses){
     var parcours = "";
-
+    
     courses.forEach(course =>{
         // console.table(coureur);
         parcours = fillCourse(course);
@@ -12,12 +14,15 @@ function loadCourse(courses){
 }
 
 function fillCourse(course){
-    var chemin = '<tr><th>'+course['id']+'</th><td>'+course['libelle']+'</td><td>'+course['date']+'</td><td>'+course['nb_tour']+'</td><td>'+course['distance']+'</td><td>'+course['nb_coureur']+'</td><td>'+course['longueur_tour']+'</td><td>'+course['club']+'</td><td><button class="button" name="bouton" id="'+course['id']+'" value="'+course['id']+'">Voir +</button></td></tr>';
-    // console.log(chemin);
+    var chemin = '<tr><th>'+course['id']+'</th><td>'+course['libelle']+'</td><td>'+course['date']+'</td><td>'+course['nb_tour']+'</td><td>'+course['distance']+'</td><td>'+course['nb_coureur']+'</td><td>'+course['longueur_tour']+'</td><td>'+course['club']+'</td><td><button class="button" name="bouton" value='+course['id']+' id="'+course['id']+'">Voir +</button></td></tr>';
+    //console.log(chemin);
     return chemin;
 }
 
 
 $('#courses').on('click', 'button', () => {
-    console.log($(event.target).attr('value'));
+    //console.log($(event.target).attr('value'));
+    if($(event.target).attr('value')==1){
+        console.log('test');
+    }
 });
