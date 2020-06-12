581db67e9b43801f41515b2a3e36c4caa6a7d7cd
WeynAmarock <64893257+WeynAmarock@users.noreply.github.com>
2020-06-12 19:01:28 +0200

    Update projet_sql.sql

--- sql/projet_sql.sql
+++ sql/projet_sql.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Client :  localhost:3306
--- Généré le :  Ven 05 Juin 2020 à 17:38
--- Version du serveur :  5.7.30-0ubuntu0.18.04.1
--- Version de PHP :  7.2.24-0ubuntu0.18.04.6
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 12 juin 2020 à 19:00
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `prjwebcir2`
+-- Base de données : `prjwebcir2`
 --
 
 -- --------------------------------------------------------
@@ -27,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categorie_age` (
-  `categorie` varchar(150) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `categorie` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `categorie_age`
+-- Déchargement des données de la table `categorie_age`
 --
 
 INSERT INTO `categorie_age` (`categorie`) VALUES
@@ -54,11 +55,11 @@ INSERT INTO `categorie_age` (`categorie`) VALUES
 --
 
 CREATE TABLE `categorie_valeur` (
-  `categorie` varchar(150) CHARACTER SET latin1 NOT NULL
+  `categorie` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `categorie_valeur`
+-- Déchargement des données de la table `categorie_valeur`
 --
 
 INSERT INTO `categorie_valeur` (`categorie`) VALUES
@@ -75,13 +76,13 @@ INSERT INTO `categorie_valeur` (`categorie`) VALUES
 --
 
 CREATE TABLE `club` (
-  `club` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `mail` varchar(150) CHARACTER SET latin1 NOT NULL,
+  `club` varchar(50) NOT NULL,
+  `mail` varchar(150) NOT NULL,
   `code_insee` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `club`
+-- Déchargement des données de la table `club`
 --
 
 INSERT INTO `club` (`club`, `mail`, `code_insee`) VALUES
@@ -103,15 +104,16 @@ CREATE TABLE `course` (
   `nb_tour` double NOT NULL,
   `distance` double NOT NULL,
   `nb_coureur` int(11) NOT NULL,
-  `longueur_tour` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `longueur_tour` double NOT NULL,
+  `club` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `course`
+-- Déchargement des données de la table `course`
 --
 
-INSERT INTO `course` (`id`, `libelle`, `date`, `nb_tour`, `distance`, `nb_coureur`, `longueur_tour`) VALUES
-(1, 'Course Cycliste FSGT à GOUESNOU (29)', '2019-05-20', 12, 87.6, 50, 7.3);
+INSERT INTO `course` (`id`, `libelle`, `date`, `nb_tour`, `distance`, `nb_coureur`, `longueur_tour`, `club`) VALUES
+(1, 'Course Cycliste FSGT à GOUESNOU (29)', '2019-05-20', 12, 87.6, 50, 7.3, 'AC GOUESNOU');
 
 -- --------------------------------------------------------
 
@@ -120,24 +122,23 @@ INSERT INTO `course` (`id`, `libelle`, `date`, `nb_tour`, `distance`, `nb_coureu
 --
 
 CREATE TABLE `cycliste` (
-  `mail` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `nom` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `prenom` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
   `num_licence` int(11) DEFAULT NULL,
   `date_naissance` date DEFAULT NULL,
   `valide` tinyint(1) DEFAULT NULL,
-  `club` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `club` varchar(50) NOT NULL,
   `code_insee` int(11) NOT NULL,
-  `categorie` varchar(150) CHARACTER SET latin1 NOT NULL,
-  `categorie_categorie_valeur` varchar(150) CHARACTER SET latin1 NOT NULL
+  `categorie` varchar(150) NOT NULL,
+  `categorie_valeur` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `cycliste`
+-- Déchargement des données de la table `cycliste`
 --
 
-INSERT INTO `cycliste` (`mail`, `nom`, `prenom`, `num_licence`, `date_naissance`, `valide`, `club`, `code_insee`, `categorie`, `categorie_categorie_valeur`) VALUES
-('  pc@uti.fr', 'PREVOT', 'Christophe', 55654078, NULL, 1, 'CC BOURG BLANC', 29061, 'Super Vétéran', '3eme  cat'),
+INSERT INTO `cycliste` (`mail`, `nom`, `prenom`, `num_licence`, `date_naissance`, `valide`, `club`, `code_insee`, `categorie`, `categorie_valeur`) VALUES
 ('ac@fgt.com', 'ABIVEN', 'Christophe', 55602886, NULL, NULL, 'CC BOURG BLANC', 29061, 'Sénior', '2eme  cat'),
 ('ac@hgt.fr', 'ARNAUD', 'Cédric', 55544762, NULL, 1, 'AC GOUESNOU', 29061, 'Sénior', '2eme  cat'),
 ('cl@team.gt', 'COLLET', 'Louis', 695576, NULL, NULL, 'ABC PLOUESCAT', 29061, 'Vétéran', '2eme  cat'),
@@ -147,6 +148,7 @@ INSERT INTO `cycliste` (`mail`, `nom`, `prenom`, `num_licence`, `date_naissance`
 ('lg@team.fr', 'LE GLEAU', 'Alain', 55477384, NULL, 1, 'AC GOUESNOU', 29015, 'Sénior', '1ere cat'),
 ('md@ilg.com', 'MOYSAN', 'David', 55537517, NULL, 1, 'ABC PLOUESCAT', 29015, 'Super Vétéran', '2eme  cat'),
 ('my@trez.fr', 'MEVEL', 'Yann', 369676, NULL, 1, 'AC GOUESNOU', 29061, 'Vétéran', '1ere cat'),
+('pc@uti.fr', 'PREVOT', 'Christophe', 55654078, NULL, 1, 'CC BOURG BLANC', 29061, 'Super Vétéran', '3eme  cat'),
 ('sjm@rest.ft', 'SALIOU', 'Jean Marc', 135645, NULL, 1, 'AC GOUESNOU', 29185, 'Vétéran', '2eme  cat'),
 ('tb@opf.com', 'TIRILLY', 'Bertrand', 674243, NULL, NULL, 'SAINT RENAN.I.V', 29260, 'Vétéran', '5eme  cat');
 
@@ -157,24 +159,25 @@ INSERT INTO `cycliste` (`mail`, `nom`, `prenom`, `num_licence`, `date_naissance`
 --
 
 CREATE TABLE `participe` (
-  `mail` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `mail` varchar(100) NOT NULL,
   `id` int(11) NOT NULL,
-  `place` varchar(15) CHARACTER SET latin1 NOT NULL,
-  `dossart` varchar(15) CHARACTER SET latin1 NOT NULL,
-  `point` int(11) NOT NULL
+  `place` varchar(15) DEFAULT NULL,
+  `dossard` varchar(15) DEFAULT NULL,
+  `point` int(11) DEFAULT NULL,
+  `temps` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `participe`
+-- Déchargement des données de la table `participe`
 --
 
-INSERT INTO `participe` (`mail`, `id`, `place`, `dossart`, `point`) VALUES
-('ac@fgt.com', 1, '1', '55', 15),
-('ac@hgt.fr', 1, '2', '76', 10),
-('cl@team.gt', 1, '3', '81', 8),
-('dj@taem.com', 1, '4', '55', 7),
-('kc@pli.fr', 1, '5', '21', 4),
-('lb@team.fr', 1, '6', '18', 3);
+INSERT INTO `participe` (`mail`, `id`, `place`, `dossard`, `point`, `temps`) VALUES
+('ac@fgt.com', 1, '1', '55', 15, '02:04:04'),
+('ac@hgt.fr', 1, '2', '76', 10, '02:07:04'),
+('cl@team.gt', 1, '3', '81', 8, '02:14:04'),
+('dj@taem.com', 1, '4', '55', 7, '02:24:04'),
+('kc@pli.fr', 1, '5', '21', 4, '02:34:04'),
+('lb@team.fr', 1, '6', '18', 3, '02:44:04');
 
 -- --------------------------------------------------------
 
@@ -183,15 +186,15 @@ INSERT INTO `participe` (`mail`, `id`, `place`, `dossart`, `point`) VALUES
 --
 
 CREATE TABLE `user` (
-  `mail` varchar(150) CHARACTER SET latin1 NOT NULL,
-  `nom` varchar(150) CHARACTER SET latin1 NOT NULL,
-  `prenom` varchar(150) CHARACTER SET latin1 NOT NULL,
-  `password` varchar(150) CHARACTER SET latin1 NOT NULL,
+  `mail` varchar(150) NOT NULL,
+  `nom` varchar(150) NOT NULL,
+  `prenom` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
   `admin` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `user`
+-- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`mail`, `nom`, `prenom`, `password`, `admin`) VALUES
@@ -213,7 +216,7 @@ CREATE TABLE `ville` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `ville`
+-- Déchargement des données de la table `ville`
 --
 
 INSERT INTO `ville` (`code_insee`, `ville`, `code_postal`) VALUES
@@ -223,7 +226,7 @@ INSERT INTO `ville` (`code_insee`, `ville`, `code_postal`) VALUES
 (29260, 'ST RENAN', '29290');
 
 --
--- Index pour les tables exportées
+-- Index pour les tables déchargées
 --
 
 --
@@ -250,7 +253,8 @@ ALTER TABLE `club`
 -- Index pour la table `course`
 --
 ALTER TABLE `course`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_club_FK` (`club`);
 
 --
 -- Index pour la table `cycliste`
@@ -260,7 +264,7 @@ ALTER TABLE `cycliste`
   ADD KEY `cycliste_club_FK` (`club`),
   ADD KEY `cycliste_ville0_FK` (`code_insee`),
   ADD KEY `cycliste_categorie_age1_FK` (`categorie`),
-  ADD KEY `cycliste_categorie_valeur2_FK` (`categorie_categorie_valeur`);
+  ADD KEY `cycliste_categorie_valeur2_FK` (`categorie_valeur`);
 
 --
 -- Index pour la table `participe`
@@ -282,7 +286,7 @@ ALTER TABLE `ville`
   ADD PRIMARY KEY (`code_insee`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
@@ -290,8 +294,9 @@ ALTER TABLE `ville`
 --
 ALTER TABLE `course`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- Contraintes pour les tables exportées
+-- Contraintes pour les tables déchargées
 --
 
 --
@@ -302,11 +307,17 @@ ALTER TABLE `club`
   ADD CONSTRAINT `club_ville0_FK` FOREIGN KEY (`code_insee`) REFERENCES `ville` (`code_insee`);
 
 --
+-- Contraintes pour la table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `course_club_FK` FOREIGN KEY (`club`) REFERENCES `club` (`club`);
+
+--
 -- Contraintes pour la table `cycliste`
 --
 ALTER TABLE `cycliste`
   ADD CONSTRAINT `cycliste_categorie_age1_FK` FOREIGN KEY (`categorie`) REFERENCES `categorie_age` (`categorie`),
-  ADD CONSTRAINT `cycliste_categorie_valeur2_FK` FOREIGN KEY (`categorie_categorie_valeur`) REFERENCES `categorie_valeur` (`categorie`),
+  ADD CONSTRAINT `cycliste_categorie_valeur2_FK` FOREIGN KEY (`categorie_valeur`) REFERENCES `categorie_valeur` (`categorie`),
   ADD CONSTRAINT `cycliste_club_FK` FOREIGN KEY (`club`) REFERENCES `club` (`club`),
   ADD CONSTRAINT `cycliste_ville0_FK` FOREIGN KEY (`code_insee`) REFERENCES `ville` (`code_insee`);
 
@@ -316,6 +327,7 @@ ALTER TABLE `cycliste`
 ALTER TABLE `participe`
   ADD CONSTRAINT `participe_course0_FK` FOREIGN KEY (`id`) REFERENCES `course` (`id`),
   ADD CONSTRAINT `participe_cycliste_FK` FOREIGN KEY (`mail`) REFERENCES `cycliste` (`mail`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
